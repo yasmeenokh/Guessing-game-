b55908158c83f355718b47b92472fda6621f9903
yasmeenokh <ykhatib44@gmail.com>
2021-02-18 12:27:26 +0200

    fixed the while loops

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ let courseCompletion = prompt('Do you think i will finish this course?');
 // console.log(courseCompletion); 
 
 courseCompletion = courseCompletion.toLowerCase()
+while (courseCompletion !== 'yes' && courseCompletion !== 'y' && courseCompletion !=='no' && courseCompletion !=='n' ) {
+    courseCompletion = prompt('Do you think i will finish this course?');
+}
 
 if (courseCompletion === 'yes' || courseCompletion === 'y') {
     alert('Thank you for your good expectation 😄');
@@ -19,12 +22,13 @@ if (courseCompletion === 'yes' || courseCompletion === 'y') {
 if (courseCompletion === 'no' || courseCompletion === 'n') {
     alert('Ohh, why do you think that 😔!!');
 }
-while (courseCompletion !== 'yes' && courseCompletion !== 'y' && courseCompletion !=='no' && courseCompletion !=='n' ) {
-    courseCompletion = prompt('Do you think i will finish this course?');
-}
+
 let gamingLove = prompt('Do you think i enjoy gaming?');
 // console.log(gamingLove);
 
+while (gamingLove.toLowerCase() !== 'yes' && gamingLove.toLowerCase() !== 'y' && gamingLove.toLowerCase() !=='no' && gamingLove.toLowerCase() !=='n' ) {
+    gamingLove = prompt('Do you think i enjoy gaming?');
+}
 if (gamingLove.toLowerCase() === 'yes' || gamingLove.toLowerCase() === 'y') {
     alert('You are so mistaken');
  
@@ -33,12 +37,14 @@ if (gamingLove.toLowerCase() === 'no' || gamingLove.toLowerCase() === 'n') {
     alert('i am glad you had that right');
     counter++;
 }
-while (gamingLove.toLowerCase() !== 'yes' && gamingLove.toLowerCase() !== 'y' && gamingLove.toLowerCase() !=='no' && gamingLove.toLowerCase() !=='n' ) {
-    gamingLove = prompt('Do you think i enjoy gaming?');
-}
+
 
 let firstImp = prompt('Did i leave a good first impression?');
 // console.log(firstImp);
+
+while (firstImp.toLowerCase() !== 'yes' && firstImp.toLowerCase() !== 'y' && firstImp.toLowerCase() !=='no' && firstImp.toLowerCase() !=='n' ) {
+    firstImp = prompt('Did i leave a good first impression?');
+}
 
 if (firstImp.toLowerCase() === 'yes' || firstImp.toLowerCase() === 'y') {
     alert('Glad to hear that, it was mutual 🙈');
@@ -48,13 +54,15 @@ if (firstImp.toLowerCase() === 'yes' || firstImp.toLowerCase() === 'y') {
 if (firstImp.toLowerCase() === 'no' || firstImp.toLowerCase() === 'n') {
     alert('sorry to hear that, where did i go wrong 💔?');
 }
-while (firstImp.toLowerCase() !== 'yes' && firstImp.toLowerCase() !== 'y' && firstImp.toLowerCase() !=='no' && firstImp.toLowerCase() !=='n' ) {
-    firstImp = prompt('Did i leave a good first impression?');
-}
+
 
 
 let myPhobia = prompt('To your knowledge do i have any phobias?');
 // console.log(myPhobia); 
+
+while (myPhobia.toLowerCase() !== 'yes' && myPhobia.toLowerCase() !== 'y' && myPhobia.toLowerCase() !=='no' && myPhobia.toLowerCase() !=='n' ) {
+    myPhobia = prompt('To your knowledge do i have any phobias?');
+}
 
 if (myPhobia.toLowerCase() === 'yes' || myPhobia.toLowerCase() === 'y') {
     alert('Intersting 🙀, how did you know?');
@@ -64,14 +72,12 @@ if (myPhobia.toLowerCase() === 'no' || myPhobia.toLowerCase() === 'n') {
     alert('Unfortunately not true');
 }
 
-while (myPhobia.toLowerCase() !== 'yes' && myPhobia.toLowerCase() !== 'y' && myPhobia.toLowerCase() !=='no' && myPhobia.toLowerCase() !=='n' ) {
-    myPhobia = prompt('To your knowledge do i have any phobias?');
-}
-
-
 let tryNew = prompt('Do i like to try new things?');
 // console.log(tryNew);
 
+while (tryNew.toLowerCase() !== 'yes' && tryNew.toLowerCase() !== 'y' && tryNew.toLowerCase() !=='no' && tryNew.toLowerCase() !=='n' ) {
+    tryNew = prompt('Do i like to try new things?');
+}
 if (tryNew.toLowerCase() === 'yes' || tryNew.toLowerCase() === 'y') {
     alert('you got that right');
     counter++;
@@ -80,9 +86,7 @@ if (tryNew.toLowerCase() === 'no' || tryNew.toLowerCase() === 'n') {
     alert('how else can we learn!');
 }
 
-while (tryNew.toLowerCase() !== 'yes' && tryNew.toLowerCase() !== 'y' && tryNew.toLowerCase() !=='no' && tryNew.toLowerCase() !=='n' ) {
-tryNew = prompt('Do i like to try new things?');
-}
+
 
 let myAge = prompt('How old do you think i am');
 
@@ -95,13 +99,15 @@ for (let i = 0; i < 3; i++) {
      if (myAge < 25) {
         alert('too low , but thank you 🙈');
         myAge = prompt('How old do you think i am');
-    }
-}    if (myAge == 25) {
+    } 
+
+}    
+
+if  (myAge == 25) {
     alert ('good guess');
-    counter++;
+    counter++; 
+
 } 
-
-
 let favFood = ['oriental', 'chinese','indian'] 
 let favFoodAns = prompt ( 'what is my favourite type of food?'); 
 for (let i = 0; i < 5; i++) {
